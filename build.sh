91fcd1ddc39f10375180c65a12c08e6ace3c3701
Sani Yadav <saniyadav7755@gmail.com>
2025-08-16 19:11:41 +0530

    Fix deployment paths in build.sh and render.yaml

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python portfolio/manage.py collectstatic --no-input
-python portfolio/manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py migrate
